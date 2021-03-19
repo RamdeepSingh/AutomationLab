da2fc62ef02bb9b2ebb060fb55d0795959499e53
RamdeepSingh <ramdeepmansa@gmail.com>
2021-03-19 11:27:28 +0530

    pilot commit

---
+++ src/test/java/com/facebook/seleniumTesting/uiTest.java
@@ -0,0 +1,24 @@
+package com.facebook.seleniumTesting;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class uiTest 
+{
+
+    @Test
+    public void login()
+    {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\ramdsing\\eclipse-workspace\\systemTesting\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.strava.com/");
+		
+
+		
+    }
+}
